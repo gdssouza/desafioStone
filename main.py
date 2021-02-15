366b01d9537b29d509ad000e62262cf09be1d611
Gustavo Souza <gdssouza@id.uff.br>
2021-02-15 13:28:20 -0300

    Criando validação

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+def validaListas(lista_compras, lista_emails):
+    if lista_compras == []:
+        return False, 'A lista de compras está vazia.'
+    elif lista_emails == []:
+        return False, 'A lista de emails está vazia.'
+    elif len(lista_compras) != len(lista_emails):
+        return False, 'As listas não possuem o mesmo tamanho'
+    else:
+        return True, ''
+
 def desafio(lista_compras, lista_emails):
-    return None
+    result, msg = validaListas(lista_compras, lista_emails)
+    if result == False:
+        return msg
+    else:
+        return {}
+
+print(desafio([],[1]))
+print(desafio([1],[]))
+print(desafio([],[]))
+print(desafio([0],[1,2]))
