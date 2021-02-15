8a1f31b29fbd3c38e8278f3ce802e859afde7534
Gustavo Souza <gdssouza@id.uff.br>
2021-02-15 16:52:44 -0300

    Comentando script e aprimorando validação

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+# função de validação dos dados\
 def validaListas(lista_compras, lista_emails):
     if lista_compras == []:
         return False, 'A lista de compras está vazia.'
@@ -7,10 +8,23 @@ def validaListas(lista_compras, lista_emails):
         return False, 'A lista de compras não é 3xN'
     elif not( len(lista_compras[0]) == len(lista_compras[1]) == len(lista_compras[2])):
         return False, 'A lista de compras não é uma matriz'
-    else:
-        return True, ''
+    
+    # percorrendo cada elemento e verificando se há algum elemento no qual o tipo
+    # de dado é diferente do determinado pelo enunciado
+    itens, qtds, precos = lista_compras    
+    for tipo in list(map(type, precos)):
+        if tipo != float and tipo != int:
+            return False, 'A lista de compras possui preços que não são números'
+    if list(map(type, qtds)) != [int]*len(qtds):
+        return False, 'A lista de compras possui pelo menos um produto com quantidade não inteira.'
+    elif list(map(type, lista_emails)) != [str]*len(lista_emails):
+        return False, 'A lista de emails não é completamente composta por strings'
+    
+    # se tudo estiver ok, retorna True sem mensagem de erro
+    return True, ''
 
 def retornaSoma(lista_compras):
+    # calcula o valor total percorrendo a lista de compras
     itens, qtds, precos = lista_compras
     soma = 0
     for i in range(0, len(itens)):
@@ -25,7 +39,7 @@ def fazDivisao(soma, lista_emails):
     
     for email in lista_emails:
         mapa[email] = divisao_cents
-        
+    
     if (len_emails%2 != 0) and (len_emails > 2):
         mapa[lista_emails[-1]] += soma_cents - divisao_cents*len_emails
         
@@ -54,6 +68,7 @@ class Desafio:
         self.listaEmails_ = lista_emails
         
     def retornaMapa(self):
+        # função que faz o que é pedido no desafio
         '''
         
 
